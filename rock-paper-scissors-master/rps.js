ccbdd2cfb38c1529704fdd4e09d4a66bc3a18f1d
samjyi95 <samjyi95@gmail.com>
2020-10-06 11:54:54 -0700

    javascript DOM Manipulation in progress

--- rock-paper-scissors-master/rps.js
+++ rock-paper-scissors-master/rps.js
@@ -1,5 +1,41 @@
+var paper = document.getElementById('paper')
+var rock = document.getElementById('rock')
+var scissors = document.getElementById('scissors')
+//maybe create a class that grabs all the elems of the gameBoard (rock, paper, scissor, triangle)??
+
 //this is for dom manipulation to change the board when a choice is picked
 const something = document.querySelector('#triangle')
+
+//click events for each choice 
+paper.addEventListener('click', gamePlay)
+rock.addEventListener('click', gamePlay)
+scissors.addEventListener('click', gamePlay)
+
+//rather than a checkWin function I should have it go to gameplay functino that manipulates the DOM?
+//OR i could manipulate the DOM and checking the win condtions all in the checkWin? 
+
+function gamePlay() {
+//setting the whole gameboard so we can manip. based on player's choice 
+    var gameBoard = document.getElementById('container2') 
+    
+    //setting the clicked icon to be the DOM manipulated choice so we can set the choice to be shown
+    var chose = this.id
+    var player1 = this.innerHTML
+    document.getElementById('container2').innerHTML = this.innerHTML
+    
+    //I need to grab the attribute and set the attribute as the new gameboard within this function 
+    //var choice = chose.
+    //set choice as the selected icon to be displayed "with styling as well" *make room for house's choice
+    //console.log(choice)
+    console.log(player1) //not sure why .content is coming up as undefined resaerch more into this 
+    console.log(chose)
+    
+}
+
+//Gameplay function 
+function checkWin() {
+    //console.log('your event listens are triggering the checkWin function', event.target.id )
+}
 
 
 // Get the modal
@@ -17,10 +53,10 @@ btn.onclick = function () {
 span.onclick = function () {
     modal.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
+
