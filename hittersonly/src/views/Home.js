b395a842f7c27351579036b97f219a8ae5da23a7
samjyi95 <samjyi95@gmail.com>
2021-03-29 12:04:33 -0700

    saving files

--- hittersonly/src/views/Home.js
+++ hittersonly/src/views/Home.js
@@ -6,6 +6,7 @@ function Home(){
             <h1>Homepage</h1>
             <p>you can view your players</p>
             <p>edit your team and your batting lineup</p>
+            <p> Hello World</p>
         </div>
     )
 }
